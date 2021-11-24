13511c33ebc3f9b915eb1914f307336691c2aa69
piet <pprotassow@posteo.de>
2021-11-24 10:55:57 +0100

    refactored to function component

--- src/App.js
+++ src/App.js
@@ -1,82 +1,74 @@
-import React from 'react'
+import { useState } from 'react'
 import './App.css'
 
-class InputField extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            input: '',
-        }
+function InputField(props) {
+    const [input, setInput] = useState('')
+
+    const inputHandler = e => {
+        setInput(e.target.value)
     }
 
-    inputHandler = e => {
-        this.setState(state => ({ input: e.target.value }))
+    const clickHandler = () => {
+        props.onAddToList(input)
+        setInput('')
     }
 
-    clickHandler = () => {
-        this.props.onAddToList(this.state.input)
-        this.setState({input: ''})
-    }
-
-    render() {
-        return (
-            <div className=" ">
-                <input className="border-2 rounded mr-4 mt-3" onChange={this.inputHandler} value={this.state.input} />
-                <button className="text-gray-200 bg-purple-600 bg-opacity-75 rounded text-sm p-1" onClick={this.clickHandler}>Add</button>
-            </div>
-        )
-    }
+    return (
+        <div className=" ">
+            <input
+                className="border-2 rounded mr-4 mt-3"
+                onChange={inputHandler}
+                value={input}
+            />
+            <button
+                className="text-gray-200 bg-purple-600 bg-opacity-75 rounded text-sm p-1"
+                onClick={clickHandler}
+            >
+                Add
+            </button>
+        </div>
+    )
 }
 
-class TodoList extends React.Component {
-    constructor(props) {
-        super(props)
-        this.listItems = []
-    }
-
-    render() {
-        this.listItems = this.props.list.map((x, index) => (
-            <div className="flex mt-2 justify-between" key={index.toString()}>
-                <li className="pr-4">{x}</li>
-                <button className="text-gray-200 bg-purple-600 bg-opacity-75 rounded p-1" onClick={() => this.props.onDone(index)}>Done</button>
-            </div>
-        ))
-        return <ul>{this.listItems}</ul>
-    }
+function TodoList(props) {
+    const listItems = props.list.map((x, index) => (
+        <div className="flex mt-2 justify-between" key={index.toString()}>
+            <li className="pr-4">{x}</li>
+            <button
+                className="text-gray-200 bg-purple-600 bg-opacity-75 rounded p-1"
+                onClick={() => props.onDone(index)}
+            >
+                Done
+            </button>
+        </div>
+    ))
+    return <ul>{listItems}</ul>
 }
 
-class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            list:  localStorage.list ? localStorage.list.split(',') : []
-        }
+function App(props) {
+    const [list, setList] = useState(
+        localStorage.list ? localStorage.list.split(',') : []
+    )
+
+    const addItem = str => {
+        localStorage.list = [...list, str]
+        setList([...list, str])
     }
 
-    addItem = str => {
-        this.setState(state => {
-            localStorage.list = [...state.list, str]
-            return { list: [...state.list, str] }
-        })
+    const deleteItem = num => {
+        localStorage.list = [...list.slice(0, num), ...list.slice(num + 1)]
+        setList([...list.slice(0, num), ...list.slice(num + 1)])
     }
 
-    deleteItem = num => {
-        this.setState(state => {
-            localStorage.list = [...state.list.slice(0,num), ...state.list.slice(num+1)]
-            return ({
-            list:[...state.list.slice(0,num), ...state.list.slice(num+1)]
-        })})
-    }
-
-    render() {
-        return (
-            <div className="flex flex-col items-start mt-10 ml-32">
-                <h1 className="text-xl mb-3">You have {this.state.list.length} {this.state.list.length > 1 ? 'tasks' : 'task'}</h1>
-                <TodoList list={this.state.list} onDone={this.deleteItem}></TodoList>
-                <InputField onAddToList={this.addItem}></InputField>
-            </div>
-        )
-    }
+    return (
+        <div className="flex flex-col items-start mt-10 ml-32">
+            <h1 className="text-xl mb-3">
+                You have {list.length} {list.length > 1 ? 'tasks' : 'task'}
+            </h1>
+            <TodoList list={list} onDone={deleteItem}></TodoList>
+            <InputField onAddToList={addItem}></InputField>
+        </div>
+    )
 }
 
 export default App
